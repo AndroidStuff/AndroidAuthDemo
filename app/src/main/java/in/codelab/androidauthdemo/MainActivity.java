4cb85637669075494d193621cd98ab64d4efa6d1
Karthik Sirasanagandla <sirkarthik@gmail.com>
2017-06-13 22:28:38 +0530

    Implemented Logout

--- app/src/main/java/in/codelab/androidauthdemo/MainActivity.java
+++ app/src/main/java/in/codelab/androidauthdemo/MainActivity.java
@@ -6,7 +6,9 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -15,12 +17,16 @@ import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 
 public class MainActivity
         extends AppCompatActivity
         implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
 
     SignInButton signInButton;
+    private Button signOutButton;
     TextView statusTextView;
     GoogleApiClient mGoogleApiClient;
     private static final String TAG = "SignInActivity";
@@ -43,17 +49,47 @@ public class MainActivity
                 .build();
 
         statusTextView = (TextView) findViewById(R.id.status_textview);
-        signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        signInButton = (SignInButton) findViewById(R.id.gsign_in_button);
         signInButton.setOnClickListener(this);
+        signOutButton = (Button) findViewById(R.id.sign_out_button);
+        signOutButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch(v.getId()){
-            case R.id.sign_in_button:
+            case R.id.gsign_in_button:
                 signIn();
                 break;
+            case R.id.sign_out_button:
+                signOut();
+                break;
+            default:
+                Log.d(MainActivity.class.getCanonicalName(), "Unknown id clicked : "+v.getId());
+                break;
+
+
         }
+    }
+
+    private void signOut() {
+        Log.d(MainActivity.class.getCanonicalName(), "Signing out...");
+        PendingResult<Status> statusPendingResult = Auth.GoogleSignInApi.signOut(mGoogleApiClient);
+        statusPendingResult.setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(@NonNull Status status) {
+                if(status.isSuccess()) {
+                    Toast.makeText(MainActivity.this, status.getStatusMessage(), Toast.LENGTH_SHORT)
+                            .show();
+                    signOutButton.setVisibility(View.INVISIBLE);
+                    signInButton.setVisibility(View.VISIBLE);
+                    statusTextView.setText("Hello World!");
+                } else {
+                    Toast.makeText(MainActivity.this, status.getStatusMessage(), Toast.LENGTH_LONG)
+                            .show();
+                }
+            }
+        });
     }
 
     private void signIn() {
@@ -82,6 +118,8 @@ public class MainActivity
         if (result.isSuccess()) {
             GoogleSignInAccount acct = result.getSignInAccount();
             statusTextView.setText("Hello, " + acct.getDisplayName()); //pulls the user name
+            signInButton.setVisibility(View.INVISIBLE);
+            signOutButton.setVisibility(View.VISIBLE);
         } else {
         }
     }
